7d7eff51f1bf82ad473607a64e2c3272decabfdf
Katia <katia.zakharina@gmail.com>
2021-09-08 23:01:28 +0300

    feat: done count-cats task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,23 +1,16 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-/**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
- *
- * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
- *
- */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (
+    isNaN(sampleActivity) ||
+    typeof sampleActivity != 'string' ||
+    sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY
+  ) {
+    return false;
+  }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
 }
