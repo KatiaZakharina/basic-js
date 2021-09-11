c68492d9e69b3c1a411469a08eb785d6b436f733
Katia <katia.zakharina@gmail.com>
2021-09-11 20:25:00 +0300

    feat: done what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,12 +1,13 @@
 export default function getSeason(date) {
-  if(!date) return 'Unable to determine the time of year!';
-  if (!(date instanceof Date)) {
+  if (!date) return 'Unable to determine the time of year!';
+
+  const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+  try {
+    date instanceof Date;
+    date.toDateString();
+    return seasons[~~(((date.getMonth() + 1) % 12) / 3)];
+  } catch (e) {
     throw new Error('Invalid date!');
   }
-//   try {
-//     date.parse();
-//  }
-//  catch (e) {
-//   throw new Error('Invalid date!'); 
-//  }
 }
