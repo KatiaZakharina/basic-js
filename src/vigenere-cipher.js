03acf082de87a4249f8188a520edc2d82d5b59ea
Katia <katia.zakharina@gmail.com>
2021-09-12 18:18:23 +0300

    feat: done vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,59 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
- * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ * @prettier
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type ? type : true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!(message && key)) throw new Error('Incorrect arguments!');
+
+    let keyLength = key.length;
+    for (let i = key.length; i < message.length; i++) {
+      key += key[i % keyLength];
+    }
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    let result = '';
+    for (let i = 0, j = 0; i < message.length; i++, j++) {
+      let messK = message.charCodeAt(i);
+      let keyK = key.charCodeAt(i);
+      if (!(message.charCodeAt(i) > 64 && message.charCodeAt(i) < 91)) {
+        result += message[i];
+        j--;
+      } else {
+        result += String.fromCharCode(
+          ((message.charCodeAt(i) - 65 + key.charCodeAt(j) - 65) % 26) + 65,
+        );
+      }
+    }
+    return this.type ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!(message && key)) throw new Error('Incorrect arguments!');
+
+    let keyLength = key.length;
+    for (let i = key.length; i < message.length; i++) {
+      key += key[i % keyLength];
+    }
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    let result = '';
+    for (let i = 0, j = 0; i < message.length; i++, j++) {
+      let messK = message.charCodeAt(i);
+      let keyK = key.charCodeAt(i);
+      if (!(message.charCodeAt(i) > 64 && message.charCodeAt(i) < 91)) {
+        result += message[i];
+        j--;
+      } else {
+        result += String.fromCharCode(
+          ((message.charCodeAt(i) - 65 + 26 - key.charCodeAt(j) + 65) % 26) + 65,
+        );
+      }
+    }
+    return this.type ? result : result.split('').reverse().join('');
   }
 }
